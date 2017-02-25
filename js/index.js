cfd6a7073d61d40ddbb31dc9b077ea94fcd34e3d
范二青年 <1028301097@qq.com>
2017-02-25 16:23:44 +0800

    添加了index.html以及css,js,images,music等文件和文件夹

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+$(function () {
+
+    // 音乐播放
+    var music = document.getElementById("media");
+    music.play();
+    $("#audio_btn").click(function () {
+        $(this).toggleClass("rotate"); //控制音乐图标 自转或暂停(删除旋转属性)
+        if ($(this).hasClass("rotate")) {//判断div是否包含指定类名，并指定音乐是否播放
+          music.play();
+        } else {
+          music.pause();
+        }
+    })
+});
