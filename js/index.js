84272dd6946d6e6e128554c915744b5f79895b3a
范二青年 <1028301097@qq.com>
2017-03-13 21:33:12 +0800

    第一次github提交

--- js/index.js
+++ js/index.js
@@ -1,15 +1,14 @@
-
-$(function () {
-
+$(function() {
     // 音乐播放
     var music = document.getElementById("media");
     music.play();
-    $("#audio_btn").click(function () {
+    $("#audio_btn").click(function() {
         $(this).toggleClass("rotate"); //控制音乐图标 自转或暂停(删除旋转属性)
-        if ($(this).hasClass("rotate")) {//判断div是否包含指定类名，并指定音乐是否播放
-          music.play();
+        if ($(this).hasClass("rotate")) { //判断div是否包含指定类名，并指定音乐是否播放
+            music.play();
         } else {
-          music.pause();
+            music.pause();
         }
     })
+
 });
